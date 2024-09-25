7c6c16400f67948f53b41d61240ef6ceb93ad452
Farhan-Qaiser <farhanqaiser1019@gmail.com>
2024-09-25 12:56:08 +0500

    an array of users is created

--- Backend/Server.mjs
+++ Backend/Server.mjs
@@ -5,7 +5,17 @@ app.use(cors())
 app.use(express.json())
 
 const PORT = 3000
-
+let users = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'wY6jx@example.com',
+    }, {
+        id: 2,
+        name: 'Jane Doe',
+        email: 'qXqoH@example.com',
+    }
+]
 
 app.listen(PORT, () => {
     console.log(`Server is running on: http://localhost:${PORT}`)
