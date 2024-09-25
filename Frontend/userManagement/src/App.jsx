f65e6e64422076605d1e05c867537ccd5d2f2cb4
Farhan-Qaiser <farhanqaiser1019@gmail.com>
2024-09-25 13:04:39 +0500

    FetchUser function made using async/await

--- Frontend/userManagement/src/App.jsx
+++ Frontend/userManagement/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState,useEffect } from "react"
 import axios from 'axios'
 
+const API_URL = 'hhttp://localhost:3000/users'
 
 function App() {
-
-    const API_URL = 'hhttp://localhost:3000/users'
+    const fertchUsers = async () => {
+        const response = await axios.get(API_URL)
+        const content = response.data
+        
+    }
 
     return (
         <>
-
+            <h1>CRUD Application</h1>
         </>
     )
 }
